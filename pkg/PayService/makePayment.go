1f506879bd80983a7fd832c6c27f9275d5c584dc
JeffreyZhu <1624410543@qq.com>
2025-08-05 17:19:46 +0000

    old

--- pkg/PayService/makePayment.go
+++ pkg/PayService/makePayment.go
@@ -1,8 +1,8 @@
 /*
  * @Author: Jeffrey Zhu 1624410543@qq.com
  * @Date: 2025-03-31 13:41:43
- * @LastEditors: Jeffrey Zhu 1624410543@qq.com
- * @LastEditTime: 2025-03-31 13:47:32
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-08-05 17:02:20
  * @FilePath: \rainbow-pay-sdk-go\pkg\PaySdk\makePayment.go
  * @Description: File Description Here...
  *
